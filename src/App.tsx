63ef86f22dce64752955327ea5b97265bad4a5ec
sraynaud <raynaud.sn@gmail.com>
2023-07-23 15:55:17 +0200

    add rune

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
+import { useEffect, useState } from "react"
+
 import { HomeScreen } from "./components/screens/HomeScreen"
+import { GameState } from "./rune/logic"
 
 function App() {
+  const [game, setGame] = useState<GameState>()
+
+  useEffect(() => {
+    Rune.initClient({
+      onChange: ({ newGame }) => {
+        setGame(newGame)
+      },
+    })
+  }, [])
+
+  if (!game) {
+    return <div>Loading...</div>
+  }
+
   return <HomeScreen />
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import react from "@vitejs/plugin-react"
+import path from "node:path"
 import { defineConfig } from "vite"
+import rune from "vite-plugin-rune"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "",
+  plugins: [react(), rune({ logicPath: path.resolve("./src/rune/logic.ts") })],
 })
