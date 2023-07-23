387be9054a4459a9d6cd412251dae9604f8eba01
sraynaud <raynaud.sn@gmail.com>
2023-07-23 12:42:58 +0200

    add react-spring

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import { animated, useSpring } from "@react-spring/web"
 import { styled } from "styled-components"
 import { useTranslation } from "./i18n/i18n"
 import { theme } from "./theme/theme"
 
-const UITitle = styled.h1`
+const UITitle = styled(animated.h1)`
   color: ${theme.palette.primary.main};
   font-size: 1rem;
   font-weight: 700;
@@ -12,10 +13,22 @@ const UITitle = styled.h1`
 
 function App() {
   const { t } = useTranslation()
+  const [springs, api] = useSpring(() => ({
+    rotate: 0,
+  }))
+
+  const roll = () => {
+    api.start({
+      from: { rotate: 0 },
+      to: { rotate: 360 },
+    })
+  }
 
   return (
     <div>
-      <UITitle>{t.main.title}</UITitle>
+      <UITitle style={{ ...springs }} onClick={roll}>
+        {t.main.title}
+      </UITitle>
     </div>
   )
 }
